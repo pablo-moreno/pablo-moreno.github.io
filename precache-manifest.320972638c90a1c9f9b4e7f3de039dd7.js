62e63edb6c03400a1f7287125f7fecf9c45cce58
pablo-moreno <pablo-moreno@users.noreply.github.com>
2020-10-03 07:54:28 +0000

    deploy: e102fbf4c37cf2e7d4effed87a77bb631107218d

---
+++ precache-manifest.320972638c90a1c9f9b4e7f3de039dd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "811abbc49383386d55f7",
+    "url": "/css/app.137d5d67.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f851d6968223ba6b815a3cd8f6397ec4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a40b109a4248c33b2d6",
+    "url": "/js/about.385afb2d.js"
+  },
+  {
+    "revision": "811abbc49383386d55f7",
+    "url": "/js/app.b58fe4e1.js"
+  },
+  {
+    "revision": "1a8a6192cd99b109de89",
+    "url": "/js/chunk-vendors.a1e9926b.js"
+  },
+  {
+    "revision": "68f8b650747bbfd1f4f0e009639f5730",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
